da2586ae8bbed8c1c818fb87f8125538b400d7c2
Chris Bruce <chris@chrisbruce.com>
2018-12-09 20:27:20 -0800

    updates account example

--- examples/accounts.rs
+++ examples/accounts.rs
@@ -5,4 +5,9 @@ fn main() {
     let client = helium::Client::new("localhost", 4001);
     let accounts = client.list_accounts().unwrap();
     print!("found: {:?}", accounts);
+
+    if accounts.len() > 0 {
+        let account = accounts.first().unwrap();
+        print!("\nAccount: {}\nBalance: {}\n", account.name, account.balance);
+    }
 }
