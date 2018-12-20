24878a0bf00e085258c7bde5cbe273f0134246e8
Chris Bruce <chris@chrisbruce.com>
2018-12-19 19:32:13 -0800

    adds account printout to accounts example

--- examples/accounts.rs
+++ examples/accounts.rs
@@ -3,10 +3,8 @@ extern crate helium;
 fn main() {
     let client = helium::Client::new("localhost", 4001);
     let accounts = client.list_accounts().unwrap();
-    print!("found: {:?}", accounts);
-
-    if accounts.len() > 0 {
-        let account = accounts.first().unwrap();
+    //print!("found: {:?}", accounts);
+    for account in accounts {
         print!(
             "\nAccount: {}\nBalance: {}\n",
             account.name, account.balance
