7516960848e2b5211d6a40e7ba4f7cd54baa278c
Chris Bruce <chris@chrisbruce.com>
2018-12-22 23:48:50 -0800

    updates load-test example

--- examples/load-test.rs
+++ examples/load-test.rs
@@ -9,7 +9,8 @@ const POLL_INTERVAL: u64 = 10;
 fn main() {
     let node = helium::Node::new("localhost", 4001);
     let mut last_height = node.status().unwrap().chain_height;
-    //print!("Current Height: {}", last_height);
+    let accounts = node.list_accounts().unwrap();
+    seed_account = accounts.iter.filter(|a| a.name.unwrap_or("empty") == "Main");
 
     let interval = Duration::new(POLL_INTERVAL, 0);
     let task = Interval::new_interval(interval)
