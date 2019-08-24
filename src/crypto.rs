275ad72c10e533f109b145847cc65c76a55b544b
Chris Bruce <chris@chrisbruce.com>
2019-08-24 11:00:36 -0700

    adds sign/verify

--- src/crypto.rs
+++ src/crypto.rs
@@ -5,3 +5,12 @@ use bs58::{decode};
 type Signature = Vec<u8>;
 type Message = Vec<u8>;
 type Address = Vec<u8>;
+
+
+pub fn sign(msg: &Message, sk: &SecretKey) -> Signature {
+    sign::sign_detached(msg, sk)
+}
+
+pub fn verify(msg: &Message, sig: &Signature, pk: &PublicKey) -> bool {
+    sign::verify_detached(msg, sig, pk)
+}
