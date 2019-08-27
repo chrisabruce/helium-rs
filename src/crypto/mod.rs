25f8a10b8c04ee0dc5b2ac8e595d0153178fa182
Chris Bruce <chris@chrisbruce.com>
2019-08-26 20:16:58 -0700

    adds gen_key

--- src/crypto/mod.rs
+++ src/crypto/mod.rs
@@ -6,7 +6,8 @@
 //! [bip39]:(https://github.com/bitcoin/bips/blob/master/bip-0039.mediawiki)
 //! compatible.  bip39 uses the last word as a checksum and this implementation
 //! does not seem to support the same checksum calc.
-use bs58::decode;
+use base64;
+use bs58;
 use regex::Regex;
 use sha2::{Digest, Sha256};
 use sodiumoxide::crypto::sign;
@@ -26,11 +27,21 @@ pub fn verify(sig: &Signature, msg: &Message, pk: &PublicKey) -> bool {
 
 pub fn generate_mnemonic() -> String {
     let rand16 = randombytes(16);
+    println!("Random: {:?}", rand16);
     entropy_to_mnemonic(&rand16)
 }
 
 pub fn generate_keypair(mnemonic: &str) -> (PublicKey, SecretKey) {
-    unimplemented!()
+    let mut entropy = mnemonic_to_entropy(mnemonic);
+
+    let mut seed: Vec<u8> = Vec::new();
+    seed.append(&mut entropy.clone());
+    seed.append(&mut entropy);
+
+    match Seed::from_slice(&seed) {
+        Some(s) => sign::keypair_from_seed(&s),
+        _ => panic!("keypair from seed is None"),
+    }
 }
 
 /// Turns an entropy into a word list ala bip39
@@ -44,15 +55,14 @@ fn entropy_to_mnemonic(entropy: &Vec<u8>) -> String {
     let bits = format!("{}{}", entropy_bits, checksum_bits);
 
     lazy_static! {
-        static ref RE: Regex = Regex::new("(.{1,11})").unwrap();
+        static ref RE_BITS: Regex = Regex::new("(.{1,11})").unwrap();
     }
 
-    // TODO: Static This
     let wordlist_en = get_wordlist_en();
 
-    // This can be more effeciently handled with a single iter,
+    // This can be more efficiently handled with a single iter,
     // but want to stay consistent with mobile app.
-    let chunks: Vec<String> = RE
+    let chunks: Vec<String> = RE_BITS
         .find_iter(&bits)
         .map(|m| m.as_str().to_string())
         .collect();
@@ -65,15 +75,49 @@ fn entropy_to_mnemonic(entropy: &Vec<u8>) -> String {
     words.join(" ")
 }
 
-fn mnemonic_to_entropy(mnemonic: String) -> Vec<u8> {
+/// Converts a mnemonic to entropy.
+fn mnemonic_to_entropy(mnemonic: &str) -> Vec<u8> {
     let words: Vec<&str> = mnemonic.split(" ").collect();
 
     // TODO: Static This
     let wordlist_en = get_wordlist_en();
 
-    let bits = words.iter().map(|w| {
-        let idx = wordlist_en.iter().position(|&s| s == w);
-    });
+    let bits: String = words
+        .iter()
+        .map(|w| match wordlist_en.iter().position(|s| s == w) {
+            Some(idx) => format!("{:011b}", idx),
+            _ => panic!("mnemonic word not found in wordlist."),
+        })
+        .collect::<Vec<String>>()
+        .join("");
+
+    let divider_index: usize = ((bits.len() as f64 / 33.0) * 32.0).floor() as usize;
+    let (entropy_bits, checksum_bits) = bits.split_at(divider_index);
+
+    lazy_static! {
+        static ref RE_BYTES: Regex = Regex::new("(.{1,8})").unwrap();
+    }
+
+    let entropy_bytes: Vec<u8> = RE_BYTES
+        .find_iter(&entropy_bits)
+        .map(|m| binary_to_bytes(m.as_str()) as u8)
+        .collect();
+
+    assert!(!(entropy_bytes.len() < 16), "invalid checksum");
+    assert!(!(entropy_bytes.len() > 32), "invalid checksum");
+    assert!(entropy_bytes.len() % 4 == 0, "invalid checksum");
+
+    let new_checksum = derive_checksum_bits(&entropy_bytes);
+
+    assert!(checksum_bits == new_checksum, "invalid checksum");
+
+    entropy_bytes
+}
+
+/// Takes a PublicKey and converts it to an address.
+pub fn pubkey_to_address(pk: PublicKey) -> String {
+    let mut address = vec![1];
+    //address.append(pk.pub);
 
     unimplemented!()
 }
@@ -103,15 +147,20 @@ fn derive_checksum_bits(entropy: &Vec<u8>) -> String {
     hasher.input(entropy);
     let hash = hasher.result();
 
-    bytes_to_binary(&hash.as_slice().to_vec())[0..cs].to_string()
+    bytes_to_binary(&hash.as_slice().to_vec())[..cs].to_string()
 }
 
+/// returns an english wordlist for mnemonic
+/// currently, isn't static (which is slightly better for mem)
 fn get_wordlist_en() -> Vec<String> {
     // TODO: Static This
-    include_str!("wordlists/english.txt")
+    let wl: Vec<String> = include_str!("wordlists/english.txt")
         .split_whitespace()
         .map(|w| w.to_string())
-        .collect()
+        .collect();
+    debug_assert!(wl.len() == 2048, "Invalid wordlist length");
+
+    wl
 }
 
 #[cfg(test)]
@@ -122,8 +171,31 @@ mod should {
     #[test]
     fn generate_mnemonic_twelve_words() {
         let words = generate_mnemonic();
+        println!("{:?}", words);
         let list: Vec<&str> = words.split(" ").collect();
         assert_eq!(list.len(), 12);
+    }
+
+    #[test]
+    fn convert_mnemonic_to_words() {
+        let valid_entropy = vec![
+            206, 74, 172, 150, 62, 114, 97, 231, 56, 126, 145, 227, 72, 121, 66, 212,
+        ];
+        let mnemonic =
+            "soft fever cereal language champion vicious tiger split today duck expose prepare"
+                .to_string();
+        let entropy = mnemonic_to_entropy(&mnemonic);
+
+        assert_eq!(entropy, valid_entropy);
+    }
+
+    #[test]
+    fn correctly_generate_keypair() {
+        let mnemonic =
+            "soft fever cereal language champion vicious tiger split today duck expose prepare";
+        let (pk, sk) = generate_keypair(&mnemonic);
+
+        println!("{:?}", pk);
     }
 
     #[test]
